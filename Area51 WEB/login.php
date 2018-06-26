eff2a18020673b8657ff83f4c8ba6a5b8fe7acce
Nchen118 <nchen118@yahoo.com>
2018-06-26 15:20:38 +0800

    improvement

--- Area51 WEB/login.php
+++ Area51 WEB/login.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/adjustment.css"/>
         <link rel="stylesheet" href="css/webfont.css">
     </head>
     <body>
-        <h2>LOGIN</h2>
+        <form method="post">
+            <h2>LOGIN</h2>
 
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname">
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw">
 
-        <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-        </label>
-        <button type="submit">Login</button>
-        <span class="psw"><a href="fpassword">Forgot password?</a></span>
-        <span class="register"><br>Do not have account?</br><a href="register.php">Register Here!!</a></span>
-
-
+            <label><input type="checkbox" checked="checked" name="remember">Remember me</label>
+            <input type="submit" value="Login">
+            <span class="psw"><a href="fpassword">Forgot password?</a></span>
+            <span class="register"><br>Do not have account?</br><a href="register.php">Register Here!!</a></span>
+        </form>
     </body>
 </html>
