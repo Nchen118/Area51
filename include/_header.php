55c974e425acb6de46c690edddd795f094f624a8
Nchen118 <nchen118@yahoo.com>
2018-08-23 13:44:05 +0800

    Fresh Start Web Ass

---
+++ include/_header.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+ <meta charset="UTF-8">
+ <link rel="shortcut icon" href="/img/cb30d-xp4h3.png">
+ <link rel="stylesheet" href="/css/site.css">
+ <script src="/js/jquery-3.3.1.min.js"></script>
+ <script src="/js/site.js"></script>
+ 
+</head>
+<style>
+    img{
+        max-width: 100%;
+        height:auto;
+    }   
+    
+</style>
+<body background ="">
+    
+    <header>
+        <html>
+         
+          <h1>Area 51</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="#">Product</a>
+                <a href="#">About us</a>
+                <a href="#">Support</a>
+                <a href="#">login</a>
+                <a href="#">Register</a>
+            </nav>
+          
+            </html>
+    </header>
+    <main>
+     <div>
