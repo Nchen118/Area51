43dcdd86cbaefb64ffc8ac36e4e9ac856c07431a
My Computer <My Computer@ASUS>
2018-08-23 15:08:19 +0800

    alpha 1

--- include/_header.php
+++ include/_header.php
@@ -1,25 +1,21 @@
 <html>
-<head>
- <meta charset="UTF-8">
- <link rel="shortcut icon" href="/img/cb30d-xp4h3.png">
- <link rel="stylesheet" href="/css/site.css">
- <script src="/js/jquery-3.3.1.min.js"></script>
- <script src="/js/site.js"></script>
- 
-</head>
-<style>
-    img{
-        max-width: 100%;
-        height:auto;
-    }   
-    
-</style>
-<body background ="">
-    
-    <header>
-        <html>
-         
-          <h1>Area 51</h1>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="shortcut icon" href="/img/cb30d-xp4h3.png">
+        <link rel="stylesheet" href="/css/site.css">
+        <script src="/js/jquery-3.3.1.min.js"></script>
+        <script src="/js/site.js"></script>
+    </head>
+    <style>
+        img{
+            max-width: 100%;
+            height:auto;
+        }   
+    </style>
+    <body background ="">
+
+        <header>
+            <h1>Area 51</h1>
             <nav>
                 <a href="/">Home</a>
                 <a href="#">Product</a>
@@ -28,8 +24,6 @@
                 <a href="#">login</a>
                 <a href="#">Register</a>
             </nav>
-          
-            </html>
-    </header>
-    <main>
-     <div>
+        </header>
+        <main>
+            <div>
---
+++ product.php
@@ -0,0 +1,35 @@
+<?php
+include 'configLibrary.php';
+$page->title = 'Product';
+$page->header();
+?>
+
+<p>
+    <a href="#">All<a/>
+    <a href="#">Computer</a>
+    <a href="#">Keyboard</a>
+    <a href="#">Mouse</a>
+    <a href="#">Headset</a>
+
+</p>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+$page->footer();
+?>
+
+
+
+
+
+
+
