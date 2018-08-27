e3b0de4a41af88015d20c120b0cbb9e2b5512537
Nchen118 <nchen118@yahoo.com>
2018-08-27 22:42:45 +0800

    css update and php validation fix

--- about_us.php
+++ about_us.php
@@ -1,21 +1,14 @@
 <?php
 include 'configLibrary.php';
-$page->title='About Us';
+$page->title = 'About Us';
 $page->header();
 ?>
-
-<h1>About Area 51</h1>
-<p>
-    Area 51 is a world's leading retailers website for all categories of people.
-</p>
-<p> We offer selling all kind of computer brand and computer accessories which include world famous brand 
-    which is RAZER,ACER,DELL,ASUS AND MORE.
-</p>
-
-
-
-<?php 
+<div class="jumbotron">
+<h3 class="text-center text-body">About Area 51</h3><br>
+<p class="text-center text-body">Area 51 is a world's leading retailers website for all categories of people.</p>
+<p class="text-center text-body">We offer selling all kind of computer brand and computer accessories which include world famous brand which is RAZER,ACER,DELL,ASUS AND MORE.</p>
+</div>
+<?php
 $page->footer();
-
 ?>
 
