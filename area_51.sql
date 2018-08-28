e6dbda5f1b73b3e6332e3b0b822a7e154899e1e2
My Computer <My Computer@ASUS>
2018-08-28 11:59:45 +0800

    refresh the sql

--- area_51.sql
+++ area_51.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2018 at 07:37 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Aug 28, 2018 at 05:58 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,9 +40,10 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `email`) VALUES
+(100002, 'nicholaschen', '$2y$10$rHGUokm3RK787W8Lv40P3ONC015VJPRzwczz/hW0l0ZZsy903/Cle', 'nchen118@yahoo.com'),
 (100003, 'admin', '$2y$10$jR4a9a14JNKkrqFW2THgHuTDYc2qoTgvd1SinhAJ5qJ2.24leGQOi', 'nchen1181999@gmail.com'),
 (100004, 'wesly', '$2y$10$Cwuv16aRJMvyKPOVTdiO2uYg4wv4tkg1Cz7KvsPW9FJP.gzZdF8fu', 'wesly_0808@hotmail.com'),
-(100005, 'sam', '$2y$10$ndrvG1bzlihaG5s8EGHQse9zjjXTgtraEc4ayIG.ccOm0KhTVZErW', 'shixian0511@hotmail.com');
+(100005, 'sam', '$2y$10$RGns2/UiJiA4rurpDv2/x.QZ4GSJuxW4gnjlH3WuNGHggrQh82Se2', 'samcsx0511@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -63,20 +64,10 @@ CREATE TABLE `cart` (
 --
 
 CREATE TABLE `category` (
-  `cat_key` char(2) NOT NULL,
+  `cat_key` char(1) NOT NULL,
   `cat_name` varchar(50) NOT NULL,
   `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`cat_key`, `cat_name`, `description`) VALUES
-('HS', 'Headset', NULL),
-('KB', 'Keyboard', NULL),
-('LP', 'Laptop', NULL),
-('MS', 'Mouse', NULL);
 
 -- --------------------------------------------------------
 
@@ -104,7 +95,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `username`, `password`, `email`, `ph_number`, `profile_pic`, `first_name`, `last_name`, `address`, `city`, `post_code`, `state`) VALUES
-(1, 'nchen118', '$2y$10$rhQ88xZs2czSxCE5W0w5/.lUFa09Z2Q2yyXIS52EF6G6zR4pE5yBi', 'nchen118@yahoo.com', NULL, 'profile_picture.jpg', NULL, NULL, NULL, NULL, NULL, NULL);
+(1, 'nchen118', '$2y$10$rhQ88xZs2czSxCE5W0w5/.lUFa09Z2Q2yyXIS52EF6G6zR4pE5yBi', 'nchen118@yahoo.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'EXPLOOOOOSION', '$2y$10$4MGRPnsNlE3ZtR/dcwbOUe/Jan9SFpezYN3eFV9ZsZgd4urT9.Ztm', 'samcsx0511@gmail.com', NULL, 'profile_picture.jpg', NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -117,7 +109,7 @@ CREATE TABLE `product` (
   `name` varchar(100) NOT NULL,
   `description` varchar(255) NOT NULL,
   `brand` varchar(100) NOT NULL,
-  `category` char(2) NOT NULL,
+  `category` char(1) NOT NULL,
   `date` date NOT NULL,
   `price` decimal(8,2) NOT NULL,
   `photo` varchar(100) DEFAULT NULL
@@ -128,16 +120,16 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `description`, `brand`, `category`, `date`, `price`, `photo`) VALUES
-(10001, 'S5X KABYLAKE', 'The S5X comes equipped with the latest Intel Core i7-7700HQ processor and Nvidia GeForce GTX1070 8GB Max-Q graphcis in a 18.6mm thin chassis. Weighing no more than 1.9kg, your S5X is the perfect lightweight gaming companion.', 'ILLEGEAR', 'LP', '2016-09-08', '6700.00', '5b838ed51fa17.jpg'),
-(10002, 'Asus VivoBook S15 S510UN', 'The ASUS VivoBook S15 gives you the perfect combination of beauty and performance. With its slim NanoEdge bezel, brushed-metal finish, the latest IntelÂ® Coreâ„¢ i7 processor with 16GB RAM, and NVIDIAÂ® GeForceÂ® MX150 graphics*, VivoBook S15 is designed ', 'ASUS', 'LP', '2016-10-20', '2825.00', '5b839dac08833.jpg'),
-(10003, 'Dell Inspiron 15 3000 series-3576', 'Inspiron 15 Inch 3000 Laptop with in-demand features and the latest IntelÂ® processors, CinemaStream and CinemaSound.', 'DELL', 'LP', '2016-12-25', '1800.00', '5b839eeaf302d.jpg'),
-(10004, 'Dell G7 15 Gaming Laptop', '15-Inch gaming laptop designed for a powerful, immersive in-game experience featuring NVIDIAÂ® GeForceÂ® GTX 1060 graphics and the latest 8th Gen IntelÂ® Quad-and-Hex Coreâ„¢ CPUs.', 'DELL', 'LP', '2018-02-12', '3769.00', '5b83b351ded0c.jpg'),
-(10005, 'msi-GE73-Raider-RGB-8RE', 'Inspired by exotic sports cars and the RGB master race, the GE63/73 Raider RGB are given a new identity. The new lighting top cover design with Mystic Light and Gaming Mode optimization offers the fanciest illumination ever. Get ready to light up whoeverâ', 'MSI', 'LP', '2018-05-30', '7700.00', '5b83b4631a739.jpg'),
-(10006, 'MSI-GS65-STEALTH-THIN-8RF', 'GS65 Stealth Thin - An ultraportable 15.6-inch laptop with thin bezel gaming display and its brand new gold-and-black premium design. Sharp, powerful, slim - all forged into one.', 'MSI', 'LP', '2018-07-25', '8359.00', '5b83b4eccd833.jpg'),
-(10007, 'ALIENWARE 17', '17-inch gaming laptop is the ultimate powerhouse machine, designed for VR with upgraded cooling technology and a new-generation overclocked CPU.', 'Alienware', 'LP', '2017-07-26', '7549.00', '5b83b569d29cb.jpg'),
-(10008, 'PREDATOR HELIOS 300', 'Conquer the competition with the latest 8th Gen IntelÂ® Coreâ„¢ i7 processor1 and overclockable NVIDIAÂ® GeForceÂ® GTX 1060 graphics', 'Predator', 'LP', '2017-08-30', '3819.00', '5b83b8692265c.jpg'),
-(10009, 'Predator Triton 700', 'The Predator Triton 700 delivers the power of a full-on desktop replacement in the body of a stylish, portable mainstream laptop.', 'Predator', 'LP', '2018-06-24', '12200.00', '5b83b95958b25.jpg'),
-(10010, 'ROG Zephyrus M (GM501)', 'ROG Zephyrus M picks up where its predecessor, ROG Zephyrus, left off â€” delivering mighty performance in a minute package. Itâ€™s powered by up to an 8th Generation IntelÂ® Coreâ„¢ i7 processor and NVIDIAÂ® GeForceÂ® GTX 1070 graphics, and features an I', 'ROG', 'LP', '2018-08-24', '7545.00', '5b83b9df514d6.jpg');
+(10001, 'S5X KABYLAKE', 'The S5X comes equipped with the latest Intel Core i7-7700HQ processor and Nvidia GeForce GTX1070 8GB Max-Q graphcis in a 18.6mm thin chassis. Weighing no more than 1.9kg, your S5X is the perfect lightweight gaming companion.', 'ILLEGEAR', 'L', '2016-09-08', '6700.00', '5b838ed51fa17.jpg'),
+(10002, 'Asus VivoBook S15 S510UN', 'The ASUS VivoBook S15 gives you the perfect combination of beauty and performance. With its slim NanoEdge bezel, brushed-metal finish, the latest IntelÂ® Coreâ„¢ i7 processor with 16GB RAM, and NVIDIAÂ® GeForceÂ® MX150 graphics*, VivoBook S15 is designed ', 'ASUS', 'L', '2016-10-20', '2825.00', '5b839dac08833.jpg'),
+(10003, 'Dell Inspiron 15 3000 series-3576', 'Inspiron 15 Inch 3000 Laptop with in-demand features and the latest IntelÂ® processors, CinemaStream and CinemaSound.', 'DELL', 'L', '2016-12-25', '1800.00', '5b839eeaf302d.jpg'),
+(10004, 'Dell G7 15 Gaming Laptop', '15-Inch gaming laptop designed for a powerful, immersive in-game experience featuring NVIDIAÂ® GeForceÂ® GTX 1060 graphics and the latest 8th Gen IntelÂ® Quad-and-Hex Coreâ„¢ CPUs.', 'DELL', 'L', '2018-02-12', '3769.00', '5b83b351ded0c.jpg'),
+(10005, 'msi-GE73-Raider-RGB-8RE', 'Inspired by exotic sports cars and the RGB master race, the GE63/73 Raider RGB are given a new identity. The new lighting top cover design with Mystic Light and Gaming Mode optimization offers the fanciest illumination ever. Get ready to light up whoeverâ', 'MSI', 'L', '2018-05-30', '7700.00', '5b83b4631a739.jpg'),
+(10006, 'MSI-GS65-STEALTH-THIN-8RF', 'GS65 Stealth Thin - An ultraportable 15.6-inch laptop with thin bezel gaming display and its brand new gold-and-black premium design. Sharp, powerful, slim - all forged into one.', 'MSI', 'L', '2018-07-25', '8359.00', '5b83b4eccd833.jpg'),
+(10007, 'ALIENWARE 17', '17-inch gaming laptop is the ultimate powerhouse machine, designed for VR with upgraded cooling technology and a new-generation overclocked CPU.', 'Alienware', 'L', '2017-07-26', '7549.00', '5b83b569d29cb.jpg'),
+(10008, 'PREDATOR HELIOS 300', 'Conquer the competition with the latest 8th Gen IntelÂ® Coreâ„¢ i7 processor1 and overclockable NVIDIAÂ® GeForceÂ® GTX 1060 graphics', 'Predator', 'L', '2017-08-30', '3819.00', '5b83b8692265c.jpg'),
+(10009, 'Predator Triton 700', 'The Predator Triton 700 delivers the power of a full-on desktop replacement in the body of a stylish, portable mainstream laptop.', 'Predator', 'L', '2018-06-24', '12200.00', '5b83b95958b25.jpg'),
+(10010, 'ROG Zephyrus M (GM501)', 'ROG Zephyrus M picks up where its predecessor, ROG Zephyrus, left off â€” delivering mighty performance in a minute package. Itâ€™s powered by up to an 8th Generation IntelÂ® Coreâ„¢ i7 processor and NVIDIAÂ® GeForceÂ® GTX 1070 graphics, and features an I', 'ROG', 'L', '2018-08-24', '7545.00', '5b83b9df514d6.jpg');
 
 -- --------------------------------------------------------
 
@@ -210,7 +202,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `product`
