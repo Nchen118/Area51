f7fbfbefb9946ed19738884e99d7c4ee81fc89b5
Nchen118 <nchen118@yahoo.com>
2018-08-29 17:39:23 +0800

    css fix and php update

--- view_customer.php
+++ view_customer.php
@@ -1,31 +1,67 @@
 <?php
 include 'configLibrary.php';
 $page->authorize('admin');
+
+$search = '';
 $pdo = $page->pdo();
 
-if($page->is_get()){
-    $id = $page->get('id');
-    $stm= $pdo->prepare("SELECT * FROM customer where id = ?");
-    $stm->execute[($id)];
-    $a=$stm->fetchAll();
+$stm = $pdo->prepare("SELECT * FROM customer");
+$stm->execute();
+$customers = $stm->fetchAll();
+
+if ($page->is_post()) {
+    $search = $page->post('search');
+    $stm = $pdo->prepare("SELECT * FROM `customer` WHERE (id = ? OR username LIKE '%$search%' OR email LIKE '%$search%')");
+    $stm->execute([$search]);
+    $customers = $stm->fetchAll();
 }
 
-
-$page->title='View Customer';
+$page->title = 'View Customer';
 $page->header();
 ?>
-<div class="d-flex flex-wrap justify-content-center">
-    <?php foreach ($a as $p) { ?>
-        <div class="customer">
-            <a href='customer_detail.php?id=<?= $p->id ?>'>
-                <img src='/photo/<?= $p->photo ?>' width="150px" height="150px" class="mx-auto d-block">
-                <hr>
-                <div class="text-center"><?= $p->username ?></div>
-            </a>
+<form method="post">
+    <div class="wrapper form-group">
+        <div class="input-group mb-3">
+            <input type="text" name="search" class="form-control" placeholder="Search customer..." value="<?= $search ?>">
+            <div class="input-group-prepend">
+                <span type="submit" class="input-group-text"><i class="material-icons">search</i></span>
+            </div>
         </div>
-<?php } ?>
+    </div>
+</form>
+
+<h2>Customer List</h2>
+
+<div class="jumbotron text-body">
+    <table class="table table-hover table-bordered">
+        <thead class="bg-dark text-light text-center">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Option</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($customers as $v) {
+                echo "
+                    <tr>
+                        <td class='text-right'>$v->id</td>
+                        <td>$v->username</td>
+                        <td>$v->email</td>
+                        <td class='text-center'>
+                            <a href='' class='btn btn-secondary' style='display: inline;'>Edit</a>
+                            <a href='' class='btn btn-danger' style='display: inline;'>Delete</a>
+                        </td>
+                    </tr>
+                ";
+            }
+            ?>
+        </tbody>
+    </table>
 </div>
 
-<?php 
+<?php
 $page->footer();
 ?>
