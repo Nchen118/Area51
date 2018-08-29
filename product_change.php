ced4c60ef2e0e0093c32dbe4f1621b6f59abafd4
wesly12345 <wesly12345@DESKTOP-V41D2GM>
2018-08-30 01:55:34 +0800

    change_product

---
+++ product_change.php
@@ -0,0 +1,191 @@
+<?php
+include 'configLibrary.php';
+$page->authorize('admin');
+$err = array();
+$pdo = $page->pdo();
+$photo = "no-photo.png";
+$states = array(
+    "SL" => "Selangor",
+    "KL" => "Wilayah Persekutuan",
+    "SW" => "Sarawak",
+    "JH" => "Johor",
+    "PN" => "Penang",
+    "SB" => "Sabah",
+    "PR" => "Perak",
+    "PH" => "Pahang",
+    "NS" => "Negeri Sembilan",
+    "KD" => "Kedah",
+    "ML" => "Malacca",
+    "TR" => "Terengganu",
+    "KT" => "Kelantan",
+    "PL" => "Perlis"
+);
+// POST request (update) -------------------------------------------------------
+if ($page->is_post()) {
+    $name = $page->post('name');
+    $description = $page->post('description');
+    $brand = $page->post('brand');
+    $category = $page->post('category');
+    $date = $page->post('date');
+    $price = $page->post('price');
+   
+    $file = $_FILES['file']; // Photo
+    // TODO: Select photo
+    $id= $page->get('id');
+    $stm = $pdo->prepare("SELECT photo FROM `product` WHERE id = ?");
+    $stm->execute([$id]);
+    $photo = $stm->fetchColumn();
+
+    if ($name == '') {
+        $err['name'] = 'ProductName is required.';
+    }
+
+    if ($description == '') {
+        $err['description'] = 'Description is required.';
+    }
+
+    if ($brand == '') {
+        $err['brand'] = 'Brand is required.';
+    }
+
+    if ($category == '') {
+        $err['category'] = 'Category is required.';
+    }
+
+    if ($date == '') {
+        $err['date'] = 'Date required.';
+    } else if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
+        $err['date'] = 'Date format Invalid';
+    }
+
+    if ($price == '') {
+        $err['price'] = 'Price is required';
+    } else if (!preg_match('/^\d{0,5}.\d{0,2}$/', $price)) {
+        $err['price'] = 'Format Invalid';
+    }
+
+    // TODO: Validate file only if a file is uploaded
+    //       Since it is optional to replace photo
+    if ($file['name']) {
+        if ($file['error'] == UPLOAD_ERR_FORM_SIZE ||
+                $file['error'] == UPLOAD_ERR_INI_SIZE) {
+            $err['file'] = 'Photo exceeds size allowed.';
+        } else if ($file['error'] != UPLOAD_ERR_OK) {
+            $err['file'] = 'Photo failed to upload.';
+        } else {
+            // NOTE: Remember to enable "fileinfo" extension in "php.ini"
+            $mime = mime_content_type($file['tmp_name']);
+            if ($mime != 'image/jpeg' && $mime != 'image/png') {
+                $err['file'] = 'Only JPEG or PNG photo allowed.';
+            }
+        }
+    }
+    
+    if (!$err) {
+        // TODO: Update member record
+        // (1) Photo
+        if ($file['name']) {
+            // TODO: Delete old photo
+            unlink("../photo/$photo");
+
+            $photo = uniqid() . '.jpg';
+            $img = new SimpleImage();
+            $img->fromFile($file['tmp_name'])
+                    ->thumbnail(150, 150)
+                    ->toFile($page->root . "/picture/$photo", 'image/jpeg');
+
+            // TODO: Update session
+            $_SESSION['photo'] = $photo;
+        }
+
+        // (2) Update member record
+        $stm = $pdo->prepare("
+            UPDATE `product`
+            SET  name = ?, description = ?, brand = ?, category = ?, date = ?, price = ?,photo=?
+            WHERE id = ?
+        ");
+        $stm->execute([ $name, $description, $brand, $category, $date, $price,$photo, $id]);
+
+        $page->temp('success', 'Profile changed.');
+        $page->redirect('../index.php');
+    }
+}
+
+if ($page->is_get()) {
+    $id= $page->get('id');
+    $stm = $pdo->prepare("SELECT * FROM `product` WHERE id = ?");
+    $stm->execute([$id]);
+    $m = $stm->fetch();
+
+    $name = $m->name;
+    $description = $m->description;
+    $brand = $m->brand;
+    $category = $m->category;
+    $date = $m->date;
+    $price = $m->price;
+}
+
+$page->title = 'Change Profile';
+$page->header();
+?>
+<?= $page->temp('success') ?>
+<h2>Change Profile</h2>
+<form method="post" enctype="multipart/form-data" autocomplete="off">
+    <div class="jumbotron text-body">
+        <div class="row form-group">
+            <div class="col-3 text-right">Name</div>
+            <?php $html->text('name', $name, 50, 'class="col-8 text-left form-control"') ?>
+            <?php $html->err_msg($err, 'name') ?>
+        </div>        
+        <div class="row form-group">
+            <div class="col-3 text-right">Description</div>
+            <?php $html->text('description', $description, 100, 'class="col-8 text-left form-control"') ?>
+            <?php $html->err_msg($err, 'description') ?>
+        </div>
+        <div class="row form-group">
+            <div class="col-3 text-right">Category</div>
+            <input type="text" name="category" value="<?= $category ?>" class="col-3 text-left form-control" maxlength="12" placeholder="01X-XXXXXXX">
+            <?php $html->err_msg($err, 'category') ?>
+        </div>
+         <div class="row form-group">
+            <div class="col-3 text-right" placeholder="YYYY-mm-DD">Date</div>
+            <?php $html->text('date', $date, 10, 'class="col-8 text-left form-control"') ?>
+            <?php $html->err_msg($err, 'date') ?>
+        </div>
+        <div class="row form-group">
+            <div class="col-3 text-right">Price</div>
+            <?php $html->text('price', $price, 100, 'class="col-8 text-left form-control"') ?>
+            <?php $html->err_msg($err, 'price') ?>
+        </div>
+        <div class="row form-group">
+            <div class="col-3 text-right">Photo</div>
+            <label>
+                <input type="file" id="file" name="file" accept="image/*" class="form-control-file border">
+                <div>Select photo (optional)...</div>
+                <img id="prev" src="/picture/<?= $photo ?>" width="150px" height="150px" class="border border-dark">
+            </label>
+            <?php $html->err_msg($err, 'file') ?>
+        </div>
+        <div class="text-center">
+            <a href="javascript:history.back()" class="btn btn back">Back</a>
+            <button type="submit" class="btn btn-primary">Change Profile</button>
+        </div>
+    </div>
+</form>
+<script>
+    var img = $("#prev")[0];
+    var src = img.src;
+
+    img.onerror = function (e) {
+        img.src = src;
+        $("#file").val("");
+    };
+
+    $("#file").change(function (e) {
+        var f = this.files[0] || new Blob();
+        img.src = URL.createObjectURL(f);
+    });
+</script>
+<?php
+$page->footer();
+?>
--- view_product.php
+++ view_product.php
@@ -5,53 +5,50 @@ $page->authorize('admin');
 $search = '';
 $pdo = $page->pdo();
 
-$stm = $pdo->prepare("SELECT * FROM customer");
+$stm = $pdo->prepare("SELECT * FROM `product`");
 $stm->execute();
-$customers = $stm->fetchAll();
+$products = $stm->fetchAll();
 
 if ($page->is_post()) {
     $search = $page->post('search');
-    $stm = $pdo->prepare("SELECT * FROM `customer` WHERE (id=? OR username LIKE '%$search%' OR email LIKE '%$search%')");
+    $stm = $pdo->prepare("SELECT * FROM `product` WHERE (id=? OR username LIKE '%$search%' OR email LIKE '%$search%')");
     $stm->execute([$search]);
-    $customers = $stm->fetchAll();
+    $products = $stm->fetchAll();
 }
 
-$page->title = 'View Customer';
+
+
+$page->title = 'View Product';
 $page->header();
 ?>
 <form method="post">
     <div class="wrapper form-group">
         <div class="input-group mb-3">
-            <input type="text" name="search" class="form-control" placeholder="Search customer..." value="<?= $search ?>">
+            <input type="text" name="search" class="form-control" placeholder="Search Product..." value="<?= $search ?>">
             <div class="input-group-prepend">
                 <span type="submit" class="input-group-text"><i class="material-icons">search</i></span>
             </div>
         </div>
     </div>
 </form>
-
-<h2>Customer List</h2>
-
 <div class="jumbotron text-body">
     <table class="table table-hover table-bordered">
         <thead class="bg-dark text-light text-center">
             <tr>
                 <th>Id      </th>
-                <th>Username</th>
-                <th>Email</th>
+                <th>Name</th>
                 <th>Option</th>
             </tr>
         </thead>
         <tbody>
             <?php
-            foreach ($customers as $v) {
+            foreach ($products as $v) {
                 echo "
                     <tr>
                         <td>$v->id</td>
-                        <td>$v->username</td>
-                        <td>$v->email</td>
+                        <td>$v->name</td>
                         <td class='text-center'>
-                            <a href='customer_detail.php?username=$v->username' class='btn btn-secondary' style='display: inline;'>Edit</a>
+                            <a href='product_change.php?id=$v->id' class='btn btn-secondary' style='display: inline;'>Edit</a>
                             <a href='' class='btn btn-danger' style='display: inline;'>Delete</a>
                         </td>
                     </tr>
@@ -61,6 +58,9 @@ $page->header();
         </tbody>
     </table>
 </div>
+
+
+
 
 <?php
 $page->footer();
