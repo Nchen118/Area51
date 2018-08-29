451193891f3b9cfb14f996d6ecd7c8bdd5f5048e
Nchen118 <nchen118@yahoo.com>
2018-08-30 04:36:18 +0800

    css fix and php update

--- view_customer.php
+++ view_customer.php
@@ -10,19 +10,30 @@ $stm->execute();
 $customers = $stm->fetchAll();
 
 if ($page->is_post()) {
-    $search = $page->post('search');
-    $stm = $pdo->prepare("SELECT * FROM `customer` WHERE (id=? OR username LIKE '%$search%' OR email LIKE '%$search%')");
-    $stm->execute([$search]);
-    $customers = $stm->fetchAll();
+    $action = $page->post('action');
+    if ($action == 'search') {
+        $search = $page->post('search');
+        $stm = $pdo->prepare("SELECT * FROM `customer` WHERE (id = ? OR username LIKE '%$search%' OR email LIKE '%$search%')");
+        $stm->execute([$search]);
+        $customers = $stm->fetchAll();
+    } else {
+        $stm = $pdo->prepare("DELETE FROM `customer` WHERE id = ?");
+        $stm->execute([$action]);
+        $page->temp('success', 'Delete successful');
+        $page->redirect();
+    }
 }
 
 $page->title = 'View Customer';
 $page->header();
 ?>
+
+<?= $page->temp('success') ?>
 <form method="post">
     <div class="wrapper form-group">
         <div class="input-group mb-3">
             <input type="text" name="search" class="form-control" placeholder="Search customer..." value="<?= $search ?>">
+            <input type="hidden" name="action" value="search">
             <div class="input-group-prepend">
                 <span type="submit" class="input-group-text"><i class="material-icons">search</i></span>
             </div>
@@ -32,35 +43,37 @@ $page->header();
 
 <h2>Customer List</h2>
 
-<div class="jumbotron text-body">
-    <table class="table table-hover table-bordered">
-        <thead class="bg-dark text-light text-center">
-            <tr>
-                <th>Id      </th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Option</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($customers as $v) {
-                echo "
+<form method="post">
+    <div class="jumbotron text-body">
+        <table class="table table-hover table-bordered">
+            <thead class="bg-dark text-light text-center">
+                <tr>
+                    <th>Id      </th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($customers as $v) {
+                    echo "
                     <tr>
                         <td>$v->id</td>
                         <td>$v->username</td>
                         <td>$v->email</td>
                         <td class='text-center'>
                             <a href='customer_detail.php?username=$v->username' class='btn btn-secondary' style='display: inline;'>Edit</a>
-                            <a href='' class='btn btn-danger' style='display: inline;'>Delete</a>
+                            <button name='action' value='$v->id' class='btn btn-danger' style='display: inline; margin: 0;'>Delete</button>
                         </td>
                     </tr>
                 ";
-            }
-            ?>
-        </tbody>
-    </table>
-</div>
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</form>
 
 <?php
 $page->footer();
--- view_product.php
+++ view_product.php
@@ -10,10 +10,18 @@ $stm->execute();
 $products = $stm->fetchAll();
 
 if ($page->is_post()) {
-    $search = $page->post('search');
-    $stm = $pdo->prepare("SELECT * FROM `product` WHERE (id=? OR username LIKE '%$search%' OR email LIKE '%$search%')");
-    $stm->execute([$search]);
-    $products = $stm->fetchAll();
+    $action = $page->post('action');
+    if ($action == 'search') {
+        $search = $page->post('search');
+        $stm = $pdo->prepare("SELECT * FROM `product` WHERE (id = ? OR name LIKE '%$search%')");
+        $stm->execute([$search]);
+        $products = $stm->fetchAll();
+    } else {
+        $stm = $pdo->prepare("DELETE FROM `product` WHERE id = ?");
+        $stm->execute([$action]);
+        $page->temp('success', 'Delete successful');
+        $page->redirect();
+    }
 }
 
 
@@ -21,44 +29,47 @@ if ($page->is_post()) {
 $page->title = 'View Product';
 $page->header();
 ?>
+<?= $page->temp('success') ?>
 <form method="post">
     <div class="wrapper form-group">
         <div class="input-group mb-3">
             <input type="text" name="search" class="form-control" placeholder="Search Product..." value="<?= $search ?>">
+            <input type="hidden" name="action" value="search">
             <div class="input-group-prepend">
                 <span type="submit" class="input-group-text"><i class="material-icons">search</i></span>
             </div>
         </div>
     </div>
 </form>
-<div class="jumbotron text-body">
-    <table class="table table-hover table-bordered">
-        <thead class="bg-dark text-light text-center">
-            <tr>
-                <th>Id      </th>
-                <th>Name</th>
-                <th>Option</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($products as $v) {
-                echo "
+<form method="post">
+    <div class="jumbotron text-body">
+        <table class="table table-hover table-bordered">
+            <thead class="bg-dark text-light text-center">
+                <tr>
+                    <th>Id      </th>
+                    <th>Name</th>
+                    <th>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($products as $v) {
+                    echo "
                     <tr>
                         <td>$v->id</td>
                         <td>$v->name</td>
                         <td class='text-center'>
                             <a href='product_change.php?id=$v->id' class='btn btn-secondary' style='display: inline;'>Edit</a>
-                            <a href='' class='btn btn-danger' style='display: inline;'>Delete</a>
+                            <button name='action' value='$v->id' class='btn btn-danger' style='display: inline; margin: 0;'>Delete</button>
                         </td>
                     </tr>
                 ";
-            }
-            ?>
-        </tbody>
-    </table>
-</div>
-
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</form>
 
 
 
